e5ae2ae747678d564def63ee10d17c8a2509904d
Poppy <89403578+popESF@users.noreply.github.com>
2023-04-20 13:30:04 +0100

    Changing accuracy print
    Quality of Life changes, currently trains to 9.37% accuracy

--- INT2-CNN.py
+++ INT2-CNN.py
@@ -1,131 +1,79 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[2]:
-
-
-# Import necessary libraries
 import torch
 import torch.nn as nn
 import torch.optim as optim
-from torchvision import datasets, transforms
-from torch.utils.data import DataLoader
+import torchvision.datasets as datasets
+import torchvision.transforms as transforms
+
+# Define the transforms for the dataset
+transform = transforms.Compose([
+    transforms.Resize(256),
+    transforms.CenterCrop(224),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+])
+
+# Load the train and test datasets
+train_dataset = datasets.Flowers102(root="./data", split="train", transform=transform)
+test_dataset = datasets.Flowers102(root="./data", split="test", transform=transform)
+
 
 # Define the CNN architecture
-class FlowerCNN(nn.Module):
+class CNN(nn.Module):
     def __init__(self):
-        super(FlowerCNN, self).__init__()
-        
-        # Define the convolutional layers
-        self.conv1 = nn.Conv2d(3, 16, kernel_size=3, stride=1, padding=1)
-        self.conv2 = nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=1)
-        self.conv3 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
-        
-        # Define the pooling layer
-        self.pool = nn.MaxPool2d(2, 2)
-        
-        # Define the fully connected layers
-        self.fc1 = nn.Linear(64 * 28 * 28, 512)
-        self.fc2 = nn.Linear(512, 256)
-        self.fc3 = nn.Linear(256, 102)  # 102 classes in the Flowers-102 dataset
-        
-        # Define the activation and dropout layers
-        self.relu = nn.ReLU()
-        self.dropout = nn.Dropout(0.5)
+        super(CNN, self).__init__()
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1)
+        self.conv2 = nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1)
+        self.conv3 = nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1)
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.fc1 = nn.Linear(28 * 28 * 256, 1024)
+        self.fc2 = nn.Linear(1024, 102)
         
     def forward(self, x):
-        # Pass the input through the convolutional and pooling layers
-        x = self.pool(self.relu(self.conv1(x)))
-        x = self.pool(self.relu(self.conv2(x)))
-        x = self.pool(self.relu(self.conv3(x)))
-        
-        # Flatten the tensor
-        x = x.view(x.size(0), -1)
-        
-        # Pass the input through the fully connected, activation, and dropout layers
-        x = self.dropout(self.relu(self.fc1(x)))
-        x = self.dropout(self.relu(self.fc2(x)))
-        x = self.fc3(x)  # No activation and dropout for the output layer
-        
+        x = self.pool(torch.relu(self.conv1(x)))
+        x = self.pool(torch.relu(self.conv2(x)))
+        x = self.pool(torch.relu(self.conv3(x)))
+        x = x.view(-1, 28 * 28 * 256)
+        x = torch.relu(self.fc1(x))
+        x = self.fc2(x)
         return x
 
-# Define the data preprocessing steps
-data_transforms = {
-    'train': transforms.Compose([
-        transforms.Resize(224),
-        transforms.CenterCrop(224),
-        transforms.RandomHorizontalFlip(),
-        transforms.ToTensor(),
-        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-    ]),
-    'val': transforms.Compose([
-        transforms.Resize(224),
-        transforms.CenterCrop(224),
-        transforms.ToTensor(),
-        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-    ]),
-}
+# Create an instance of the CNN and move it to the device
+cnn = CNN().to(device)
 
-# Load the dataset and apply the transformations
-data_dir = 'path/to/flowers102'
-image_datasets = {x: datasets.ImageFolder(os.path.join(data_dir, x),
-                                          data_transforms[x])
-                  for x in ['train', 'val']}
-dataloaders = {x: DataLoader(image_datasets[x], batch_size=32,
-                             shuffle=True, num_workers=4)
-                  for x in ['train', 'val']}
-dataset_sizes = {x: len(image_datasets[x]) for x in ['train', 'val']}
-class_names = image_datasets['train'].classes
-
-# Create the model and define the loss function and optimizer
-model = FlowerCNN()
+# Define the loss function and the optimizer
 criterion = nn.CrossEntropyLoss()
-optimizer = optim.Adam(model.parameters(), lr=0.001)
+optimizer = optim.Adam(cnn.parameters())
 
-# Train the model
-num_epochs = 20
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-model.to(device)
-
+# Train the CNN
+batch_size = 64
+num_epochs = 10
+train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
 for epoch in range(num_epochs):
-    print('Epoch {}/{}'.format(epoch + 1, num_epochs))
-    
-    for phase in ['train', 'val']:
-        if phase == 'train':
-            model.train()
-        else:
-            model.eval()
-        
-        running_loss = 0.0
-        running_corrects = 0
-        
-        for inputs, labels in dataloaders[phase]:
-            inputs, labels = inputs.to(device), labels.to(device)
+    for i, (images, labels) in enumerate(train_loader):
+        images = images.to(device)
+        labels = labels.to(device)
+        optimizer.zero_grad()
+        outputs = cnn(images)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+        if (i+1) % 10 == 0:
+            print(f"Epoch [{epoch+1}/{num_epochs}], Step [{i+1}/{len(train_loader)}], Loss: {loss.item():.4f}")
             
-            optimizer.zero_grad()
-            
-            with torch.set_grad_enabled(phase == 'train'):
-                outputs = model(inputs)
-                _, preds = torch.max(outputs, 1)
-                loss = criterion(outputs, labels)
-                
-                if phase == 'train':
-                    loss.backward()
-                    optimizer.step()
-            
-            running_loss += loss.item() * inputs.size(0)
-            running_corrects += torch.sum(preds == labels.data)
-        
-        epoch_loss = running_loss / dataset_sizes[phase]
-        epoch_acc = running_corrects.double() / dataset_sizes[phase]
-        
-        print('{} Loss: {:.4f} Acc: {:.4f}'.format(phase, epoch_loss, epoch_acc))
-
-print('Training complete!')
-
-
-# In[ ]:
-
+# Test the CNN
+test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+cnn.eval()
+with torch.no_grad():
+    correct = 0
+    total = 0
+    for images, labels in test_loader:
+        images = images.to(device)
+        labels = labels.to(device)
+        outputs = cnn(images)
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+    print(f"Test Accuracy: {(100 * correct / total):.2f}%")
 
 
 
